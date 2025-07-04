a2985789c0e21ea91a5057052f1ce66be5042170
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-07-04 18:12:53 +0100

    'update'

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // For custom domain
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
