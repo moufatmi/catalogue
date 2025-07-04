2311a1348a97a28d3c3d5a46b4177f11687a6e3f
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-07-04 18:06:06 +0100

    Updates

---
+++ assets/product-template-D0F8FNfr.js
@@ -0,0 +1,1 @@
+const t="0",e="Deodarant",o=12,c="https://i.imgur.com/lqiYJTQ.png",a="Beauty",i={id:t,name:e,price:12,image:c,category:a};export{a as category,i as default,t as id,c as image,e as name,o as price};
